f778cafb4a73be95433e57a14d1b4fad6563b6c1
Tales <tales.beraldo@gmail.com>
2023-04-20 00:02:50 -0400

    att

--- projeto-primeflix/src/pages/Filme/index.js
+++ projeto-primeflix/src/pages/Filme/index.js
@@ -4,29 +4,34 @@ import api from '../../services/api'
 import './filme.css'
 import { toast } from 'react-toastify'
 
-function Filme(){
-  const {id} = useParams()
+function Filme() {
+  const { id } = useParams()
   const navigate = useNavigate()
 
   const [filme, setFilme] = useState({})
   const [loading, setLoading] = useState(true)
 
-  useEffect(()=> {
-    async function loadFilme(){
+
+  const backgroundImage = {
+    backgroundImage: `url('https://image.tmdb.org/t/p/original/${filme.backdrop_path}')`
+  }
+
+  useEffect(() => {
+    async function loadFilme() {
       await api.get(`/movie/${id}`, {
         params: {
           api_key: '3865179eb23043011930a51996b0312b',
           language: 'pt-BR'
         }
       })
-      .then((response) => {
-        setFilme(response.data)
-        setLoading(false)
-      })
-      .catch(() => {
-        navigate('/', { replace: true })
-        return
-      })
+        .then((response) => {
+          setFilme(response.data)
+          setLoading(false)
+        })
+        .catch(() => {
+          navigate('/', { replace: true })
+          return
+        })
 
     }
     loadFilme()
@@ -43,7 +48,7 @@ function Filme(){
 
     const hasFilme = filmesSalvos.some((filmeSalvo) => filmeSalvo.id === filme.id)
 
-    if(hasFilme){
+    if (hasFilme) {
       toast.warn('Esse filme já está na lista!')
       return
     }
@@ -52,27 +57,34 @@ function Filme(){
     localStorage.setItem('@primeflix', JSON.stringify(filmesSalvos))
     toast.success('Filme salvo com sucesso!')
   }
-  if(loading){
-    return(
+  if (loading) {
+    return (
       <div className='filme-info'>
         <h1>Carregando detalhes ...</h1>
       </div>
     )
   }
-  return(
-    <div className='filme-info'>
-      <h1>{filme.title}</h1>
-      <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
-      <h3>Sinopse</h3>
-      <span>{filme.overview}</span>
+  return (
+    <div className="filme-poster" style={backgroundImage}>
+      <div className='filme-info'>
+        <h1>{filme.title}</h1>
+        <span>{filme.genres.map((item, index, arr) => {
+          if ((index+1) < arr.length) {
+            return `${item.name}, `
+          } else {
+            return `${item.name}`
+          }
+        })}</span>
+        <h3>Sinopse</h3>
+        <span>{filme.overview}</span>
+        <strong>Avaliação: {filme.vote_average.toFixed(1)} / 10</strong>
+        <div className='area-btn'>
+          <button onClick={salvarFilme}>Salvar</button>
+          <button>
+            <a href={`https://youtube.com/results?search_query=${filme.title} trailer`} target='blank' rel='external'>Trailer</a>
+          </button>
 
-      <strong>Avaliação: {filme.vote_average.toFixed(1)} / 10</strong>
-
-      <div className='area-btn'>
-        <button onClick={salvarFilme}>Salvar</button>
-        <button>
-          <a href={`https://youtube.com/results?search_query=${filme.title} trailer`} target='blank' rel='external'>Trailer</a>
-        </button>
+        </div>
       </div>
     </div>
   )
