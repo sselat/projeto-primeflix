01d5455769205ed8db25e062262e36aea52fb3c3
Tales B. Albuquerque <101683474+sselat@users.noreply.github.com>
2023-04-20 13:17:57 -0400

    finished

--- projeto-primeflix/src/pages/Favoritos/index.js
+++ projeto-primeflix/src/pages/Favoritos/index.js
@@ -24,29 +24,29 @@ function Favoritos() {
     toast.success('Excluído com sucesso!')
   }
   return (
-    <div className='meus-filmes'>
-      <h1>Meus filmes</h1>
-
-      {filmes.length === 0 &&
-        <div>
-          <h3>Sem filmes cadastrados!</h3>
-          <Link to={'/'} className='back-btn'>Voltar</Link>
-        </div>
-      }
-
-      <ul>
-        {filmes.map(item => {
-          return (
-            <li className='filme' key={item.id}>
-              <span>{item.title}</span>
-              <div>
-                <Link to={`/filme/${item.id}`}><FaInfoCircle className='info-icon'/></Link>
-                <button onClick={() => excluirFilme(item.id)}><FaTrash className='trash-icon'/></button>
-              </div>
-            </li>
-          )
-        })}
-      </ul>
+    <div className='meus-filmes-container'>
+      <div className='meus-filmes'>
+        <h1>Meus filmes</h1>
+        {filmes.length === 0 &&
+          <div>
+            <h3>Sem filmes cadastrados!</h3>
+            <Link to={'/'} className='back-btn'>Voltar</Link>
+          </div>
+        }
+        <ul>
+          {filmes.map(item => {
+            return (
+              <li className='filme' key={item.id}>
+                <span>{item.title}</span>
+                <div>
+                  <Link to={`/filme/${item.id}`}><FaInfoCircle className='info-icon'/></Link>
+                  <button onClick={() => excluirFilme(item.id)}><FaTrash className='trash-icon'/></button>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   )
 }
